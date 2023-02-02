870f7e32fefbbc72138588a821fd3893140e05a3
go4654 <go4654@naver.com>
2023-02-02 19:42:52 +0900

    jsx, component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+// import Text from "./components/Text";
+import { Text, Text2 } from "./components/Text";
+
 function App() {
-  return <div>HI</div>;
+  return (
+    <>
+      <div>1번</div>
+      <div>2번</div>
+      {/* *JSX 문법 규칙
+      =>반드시 부모로 묶여 있어야 된다
+      =>태그는 반드시 닫혀있어야됨
+      =>태그가 비어있을때 <></> fragment태그 라고함 */}
+
+      <Text />
+      {/* =>컴포넌트 */}
+
+      <Text2 />
+    </>
+  );
 }
+
+// JSX 바깥 주석
 
 export default App;
